77c86937643aae01cdb8cb8dd623b5affe1b161f
kushagr3108 <kushagrshukla2409@gmail.com>
2025-01-20 00:07:57 +0530

    Token generated

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,17 @@ const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 dotenv.config();
 connectToDb();
-
+app.use(express.json());
 app.use(cors());
-
 
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.use('/users', userRoutes);
 module.exports = app;
